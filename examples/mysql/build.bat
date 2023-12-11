1b6c78e5367dde9b5b98b47e693556eef6081514
tkrzielSICKAG <147154470+tkrzielSICKAG@users.noreply.github.com>
2023-12-11 10:04:12 +0100

    Update build.bat
    Added comments

--- examples/mysql/build.bat
+++ examples/mysql/build.bat
@@ -1,5 +1,5 @@
-docker build -t registry.mobilisis.com/elena.krzina/docs/mysql .
-docker push registry.mobilisis.com/elena.krzina/docs/mysql
-docker save -o mysql.tar registry.mobilisis.com/elena.krzina/docs/mysql
+docker build -t registry.mobilisis.com/user/docs/mysql .
+docker push registry.mobilisis.com/user/docs/mysql
+docker save -o mysql.tar registry.mobilisis.com/user/docs/mysql
 
 timeout /t 30
