dc2d4bed76e2069c6f194ddaa8a790fc885eadd8
ekrzina <ekrzina@mobilisis.hr>
2024-02-01 12:38:41 +0100

    Added more files for C# project.

---
+++ tutorials/c-sharp-dio-led/webDioLed/Models/Dio.cs
@@ -0,0 +1,9 @@
+﻿namespace webDioLed.Models
+{
+    public class Dio
+    {
+        public int Id { get; set; }
+        public string? Duration { get; set; }
+        public System.DateTime? Whattime { get; set; }
+    }
+}
---
+++ tutorials/c-sharp-dio-led/webDioLed/Models/DioContext.cs
@@ -0,0 +1,11 @@
+﻿namespace webDioLed.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    public class DioContext: DbContext
+    {
+        public DioContext(DbContextOptions<DioContext> options) : base(options) { }
+        public DbSet<Dio> dios { get; set; } = null!;
+
+    }
+    
+}
