1cb801bf141ed33757a6eafd3a7008be3495cdf5
tkrzielSICKAG <147154470+tkrzielSICKAG@users.noreply.github.com>
2024-02-06 12:38:00 +0100

    Delete tutorials/c-sharp-dio-led directory

--- tutorials/c-sharp-dio-led/webDioLed/Models/Dio.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace webDioLed.Models
-{
-    public class Dio
-    {
-        public int Id { get; set; }
-        public string? Duration { get; set; }
-        public System.DateTime? Whattime { get; set; }
-    }
-}
